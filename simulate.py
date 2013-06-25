93abbc41c04e52e68be3278322cc8d322191cb59
Ignacio Martinez Vazquez <ignamv@gmail.com>
2013-06-24 22:50:29 -0300

    Clean up code, store RNG seed in output file

--- simulate.py
+++ simulate.py
@@ -3,7 +3,9 @@
 import scipy as sp
 import os
 from config import *
+from time import time
 
+#----------------------------- Settings -------------------------------------
 # Number of spins in a side of the grid
 side = 64
 steps = 200
@@ -13,6 +15,29 @@ steps = 200
 beta = 2.25
 # Number of spins
 N = side*side
+# Write buffer size
+buffer_size = 4096
+make_movie = True
+#----------------------------------------------------------------------------
+
+# Seed random number generator
+seed = int(time())
+sp.random.seed(seed)
+# Output filename counter
+try:
+    counter = 1 + max([int(filename[len(output_prefix):-len(output_suffix)])
+                       for filename in os.listdir(output_dir)
+                       if filename[:len(output_prefix)] == output_prefix
+                       and filename[-len(output_suffix):] == output_suffix])
+except Exception as e:
+    counter = 1
+output_filename = os.path.join(output_dir,
+                               (output_prefix+'{:05d}').format(counter))
+
+fd = open(output_filename+output_suffix, 'w', buffer_size)
+fd.write('# N={:d}\n'.format(N))
+fd.write('# beta={:f}\n'.format(beta))
+fd.write('# seed={:d}\n'.format(seed))
 grid = sp.empty((side, side), dtype=sp.byte)
 # up is 1, down is 0
 grid[:,:] = sp.random.random_integers(0, 1, size=grid.shape)
@@ -37,26 +62,11 @@ updown[0] = sp.sum(grid[:-1,:] ^ grid[1:,:]) \
           + sp.sum(grid[-1,:] ^ grid[0,:])   \
           + sp.sum(grid[:,-1] ^ grid[:,0])
 
-# Output file name
-try:
-    counter = 1 + max([int(filename[len(output_prefix):-len(output_suffix)])
-                       for filename in os.listdir(output_dir)
-                       if filename[:len(output_prefix)] == output_prefix
-                       and filename[-len(output_suffix):] == output_suffix])
-except Exception as e:
-    counter = 1
-output_filename = os.path.join(output_dir,
-                               ''.join([output_prefix, '{:05d}',
-                                        output_suffix]).format(counter))
-buffer_size = 4096
-fd = open(output_filename, 'w', buffer_size)
-fd.write('# N={:d}\n'.format(N))
-fd.write('# beta={:f}\n'.format(beta))
-
-import cv, cv2
-movieWriter = cv2.VideoWriter(output_filename+'.avi',
-                              cv.FOURCC('U','2','6','3'), 20, (side, side),
-                              False)
+if make_movie:
+    import cv, cv2
+    movieWriter = cv2.VideoWriter(output_filename+'.avi',
+                                  cv.FOURCC('U','2','6','3'), 20, (side, side),
+                                  False)
 
 for step in xrange(1,steps):
     up[step] = up[step-1]
@@ -73,7 +83,8 @@ for step in xrange(1,steps):
                 grid[i,j] = not grid[i,j]
                 up[step] += [-1, 1][grid[i,j]]
                 updown[step] += deltaupdown
-    movieWriter.write((128*grid).astype(sp.uint8))
+    if make_movie:
+        movieWriter.write((128*grid).astype(sp.uint8))
     fd.write(' '.join(str(d) for d in data[step, :])+'\n')
 fd.close()
 
