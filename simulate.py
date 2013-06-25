891450da5a91371c300dfd535952d59de35902e0
Ignacio Martinez Vazquez <ignamv@gmail.com>
2013-06-25 11:57:06 -0300

    Removed copies of options
    Deleted variables which copied the output of ArgumentParser

--- simulate.py
+++ simulate.py
@@ -28,12 +28,8 @@ parser.add_argument('-o', '--output', help='Data output filename. Default '
                     'value is the next filename of the form '
                     +output_prefix+'NNNNN'+output_suffix)
 opt = parser.parse_args()
-side = opt.length
-steps = opt.steps
-beta = opt.beta
-make_movie = opt.movie
 # Number of spins
-N = side*side
+N = opt.length*opt.length
 # Write buffer size
 buffer_size = 4096
 #----------------------------------------------------------------------------
@@ -54,13 +50,13 @@ if opt.output is None:
 
 fd = open(opt.output, 'w', buffer_size)
 fd.write('# N={:d}\n'.format(N))
-fd.write('# beta={:f}\n'.format(beta))
+fd.write('# beta={:f}\n'.format(opt.beta))
 # Write RNG seed and commit hash so this run can be repeated later, once the
 # code has changed
 fd.write('# seed={:d}\n'.format(seed))
 fd.write('# commit='+check_output(['git', 'log', '--pretty=format:%H', '-n',
                                    '1' ])+'\n')
-grid = sp.empty((side, side), dtype=sp.byte)
+grid = sp.empty((opt.length, opt.length), dtype=sp.byte)
 # up is 1, down is 0
 grid[:,:] = sp.random.random_integers(0, 1, size=grid.shape)
 # Look into the possibility of storing (up neighbours - down neighbours) for
@@ -72,7 +68,7 @@ grid[:,:] = sp.random.random_integers(0, 1, size=grid.shape)
 # Original motivation for making up and upup into slices of data was to then
 # use data.tofile
 # TODO: either use tofile for saving or make them into independent arrays
-data = sp.empty((steps, 2), dtype=sp.uint32)
+data = sp.empty((opt.steps, 2), dtype=sp.uint32)
 # Number of up spins
 up = data[:,0]
 up[0] = sp.sum(grid)
@@ -88,7 +84,7 @@ updown[0] = sp.sum(grid[:-1,:] ^ grid[1:,:]) \
           + sp.sum(grid[:,-1] ^ grid[:,0])
 
 print 'Simulating {:d} steps of a {:d}x{:d} lattice at beta={:f}'.format(
-    steps, side, side, beta)
+    opt.steps, opt.length, opt.length, opt.beta)
 print 'Writing run data to ' + opt.output
 if opt.movie:
     if opt.movie != True:
@@ -101,38 +97,39 @@ if opt.movie:
     from cv import FOURCC
     from cv2 import VideoWriter
     movieWriter = VideoWriter(video_filename, FOURCC('U','2','6','3'),
-                              20, (side, side), False)
+                              20, (opt.length, opt.length), False)
     print 'Writing movie to ' + video_filename
 
 # Number of dashes in complete progress bar
-progress_bar_size = min(20, steps)
-steps_per_dash = steps / progress_bar_size
+progress_bar_size = min(20, opt.steps)
+steps_per_dash = opt.steps / progress_bar_size
 print '['+' '*progress_bar_size+']\r[',
 stdout.flush()
-for step in xrange(1,steps):
+for step in xrange(1,opt.steps):
     up[step] = up[step-1]
     updown[step] = updown[step-1]
-    for i in range(side):
-        for j in range(side):
+    for i in range(opt.length):
+        for j in range(opt.length):
             # Calculate change in up-down neighbours if I flip out
-            deltaupdown = grid[i,(j+1)%side] + grid[i,(j-1)%side] \
-                        + grid[(i+1)%side,j] + grid[(i-1)%side,j] - 2
+            deltaupdown = grid[i,(j+1)%opt.length] + grid[i,(j-1)%opt.length] \
+                        + grid[(i+1)%opt.length,j] + grid[(i-1)%opt.length,j] \
+                        - 2
             if not grid[i,j]:
                 deltaupdown *= -1
             # Transition probability is:
             # - if ΔE < 0: 1
             # - otherwise: exp(-beta*ΔE)
             if deltaupdown <= 0 or \
-                    sp.random.random() < sp.exp(-beta*deltaupdown):
+                    sp.random.random() < sp.exp(-opt.beta*deltaupdown):
                 grid[i,j] = not grid[i,j]
                 up[step] += [-1, 1][grid[i,j]]
                 updown[step] += deltaupdown
-    if make_movie:
+    if opt.movie:
         movieWriter.write((128*grid).astype(sp.uint8))
     if step % steps_per_dash == 0:
         stdout.write('-')
         stdout.flush()
-if steps % progress_bar_size == 0:
+if opt.steps % progress_bar_size == 0:
     stdout.write('-')
     stdout.flush()
 
